ad5e55e9d44dddb8aa1d63dc9668a337e7e127f2
CoderJoe <jwilliamson@jslcodelabs.com>
2026-02-08 23:01:47 -0600

    Add LightningTablesService to check existence of lt_tables

---
+++ LightningTables-core/src/main/kotlin/services/LightningTablesService.kt
@@ -0,0 +1,15 @@
+package com.coderjoe.lightningtables.core.services
+
+import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+
+class LightningTablesService {
+
+    fun ltTablesExists(): Boolean {
+        return transaction {
+            SchemaUtils.listTables().any {
+                it == "lt_tables" || it.endsWith(".lt_tables")
+            }
+        }
+    }
+}
