cd50880316fd53bdcc24841ea1f639b41d2e0d5b
CoderJoe <jwilliamson@jslcodelabs.com>
2026-02-09 21:41:17 -0600

    Add DatabaseConnection data class for database configuration

---
+++ LightningTables-cli/src/main/kotlin/model/DatabaseConnection.kt
@@ -0,0 +1,9 @@
+package com.coderjoe.lightningtables.cli.model
+
+data class DatabaseConnection (
+    val host: String,
+    val username: String,
+    val password: String,
+    val database: String,
+    val port: Int
+)
