f6e7054d9a61090f79cdc6fffc8689fbbc075d34
CoderJoe <jwilliamson@jslcodelabs.com>
2026-02-11 00:53:48 -0600

    Refactor main function to integrate DatabaseConnectMenu and MainMenu, and add validation for application tables

--- LightningTables-cli/src/main/kotlin/Main.kt
+++ LightningTables-cli/src/main/kotlin/Main.kt
@@ -1,50 +1,54 @@
 package com.coderjoe.lightningtables.cli
 
-import com.coderjoe.lightningtables.cli.model.DatabaseConnection
-import com.coderjoe.lightningtables.core.database.DatabaseConfig
+import com.coderjoe.lightningtables.cli.menus.DatabaseConnectMenu
+import com.coderjoe.lightningtables.cli.menus.MainMenu
+import com.coderjoe.lightningtables.cli.menus.utils.ConsoleInputHelper
+import com.coderjoe.lightningtables.core.services.LightningTablesService
+
+val lightningTableService = LightningTablesService()
 
 fun main() {
-    val validatedCredentials = credentialsLoop()
+    val validatedCredentials = DatabaseConnectMenu.load()
 
     if (!validatedCredentials) {
         println("A valid database connection is required for this application to run. Exiting.")
         return
     }
+
+    validateApplicationTablesExist()
+
+    MainMenu.load()
 }
 
-fun credentialsLoop(tryCount: Int = 0): Boolean {
-    var tryCount = tryCount
+fun validateApplicationTablesExist() {
+    println("Checking if application tables exist")
 
-    if (tryCount >= 3) {
-        println("Too many failed attempts. Exiting.")
-        return false
+    if (!lightningTableService.ltTablesExists()) {
+        println("""
+            You can use this application without the application tables
+            but you will not be able to delete or manage any LT tables created.
+            You will only be able to create new LT tables.
+            If you choose not to create the application tables,
+            you can create them later by running this application again.
+        """)
+
+        when (ConsoleInputHelper.getInputWithLabel("No lightning tables exist. Would you like to create them? (y/n)")) {
+            "y" -> {
+                println("Creating tables...")
+                lightningTableService.createLtTables()
+                if (!lightningTableService.ltTablesExists()) {
+                    println("Failed to create tables. Exiting.")
+                    return
+                }
+                println("Tables created successfully.")
+            }
+            else -> {
+                println("""
+                    You will not be able to manage or delete any LT tables
+                    created without the application tables.
+                    You can create the application tables later by running this application again.
+                """)
+            }
+        }
     }
-
-    try {
-        val credentials =
-            DatabaseConnection(
-                host = getInput("Host: "),
-                username = getInput("Username: "),
-                password = getInput("Password: "),
-                database = getInput("Database: "),
-                port = getInput("Port: ").toInt(),
-            )
-
-        DatabaseConfig.initialize(
-            url = "jdbc:mariadb://${credentials.host}:${credentials.port}/${credentials.database}",
-            username = credentials.username,
-            password = credentials.password,
-        )
-
-        return true
-    } catch (e: Exception) {
-        println("Error: ${e.message}")
-        tryCount++
-        return credentialsLoop(tryCount)
-    }
-}
-
-fun getInput(title: String): String {
-    println(title)
-    return readln()
 }
