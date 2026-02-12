260854dc7135702ba5dbc9b3e046b96bc4ee8b48
CoderJoe <jwilliamson@jslcodelabs.com>
2026-02-12 00:41:34 -0600

    Add lightning table creation and backfill functionality to MainMenu

--- LightningTables-cli/src/main/kotlin/menus/MainMenu.kt
+++ LightningTables-cli/src/main/kotlin/menus/MainMenu.kt
@@ -1,22 +1,38 @@
 package com.coderjoe.lightningtables.cli.menus
 
 import com.coderjoe.lightningtables.cli.menus.utils.ConsoleInputHelper
+import com.coderjoe.lightningtables.core.services.BackfillService
+import com.coderjoe.lightningtables.core.services.LightningTableTriggerGeneratorSqlParser
+import com.coderjoe.lightningtables.core.services.LightningTablesService
 
 object MainMenu {
+    val sqlParser = LightningTableTriggerGeneratorSqlParser()
+    val lightningTableService = LightningTablesService()
+
     fun load() {
         var exit = false
 
         while (!exit) {
-            val input = ConsoleInputHelper.getInputWithLabel("""
+            val input =
+                ConsoleInputHelper.getInputWithLabel(
+                    """
                     Menu:
                     1. Create new lightning table
                     2. List lightning tables
                     3. Exit
-                """)
+                """,
+                )
 
             when (input) {
                 "1" -> {
-                    println("Creating LT Table")
+                    val query = ConsoleInputHelper.getInputWithLabel("Enter query to create lightning table: ")
+                    val tableName = ConsoleInputHelper.getInputWithLabel("Enter name for the lightning table: ")
+                    val result = sqlParser.generate(query, tableName)
+
+                    lightningTableService.createLightningTable(result)
+                    lightningTableService.insert(result, query)
+
+                    BackfillService().backfill(result.backfillContext, result.triggers.values.toList())
                 }
                 "2" -> {
                     println("Listing LT Tables")
