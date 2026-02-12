841525e3e7fb103440e706cc72f9196cac054853
CoderJoe <jwilliamson@jslcodelabs.com>
2026-02-12 00:41:07 -0600

    spotless formatting

--- LightningTables-cli/src/main/kotlin/Main.kt
+++ LightningTables-cli/src/main/kotlin/Main.kt
@@ -24,13 +24,15 @@ fun validateApplicationTablesExist() {
     println("Checking if application tables exist")
 
     if (!lightningTableService.ltTablesExists()) {
-        println("""
+        println(
+            """
             You can use this application without the application tables
             but you will not be able to delete or manage any LT tables created.
             You will only be able to create new LT tables.
             If you choose not to create the application tables,
             you can create them later by running this application again.
-        """)
+        """,
+        )
 
         when (ConsoleInputHelper.getInputWithLabel("No lightning tables exist. Would you like to create them? (y/n)")) {
             "y" -> {
@@ -43,11 +45,13 @@ fun validateApplicationTablesExist() {
                 println("Tables created successfully.")
             }
             else -> {
-                println("""
+                println(
+                    """
                     You will not be able to manage or delete any LT tables
                     created without the application tables.
                     You can create the application tables later by running this application again.
-                """)
+                """,
+                )
             }
         }
     }
